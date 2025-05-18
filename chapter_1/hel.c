a771d9334b57f6486c8ade0d5de343fda620865f
Shivam-Jais <sivamjaiswal322@gmail.com>
2025-05-18 12:38:08 +0530

    Add files via upload

---
+++ chapter_1/hel.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main (){
+    printf("suchii");
+    return 0 ;
+    
+}
---
+++ chapter_1/hello.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main (){
+   /*  printf("hello shivam \n");
+     printf("hello shivam \n"); // we are using backslan n (\n) for next line 
+     printf("hello shivam \n\n");
+     printf("hello shivam\n");
+     return 0 ;*/
+
+    /* int age = 22 ;
+     float pi = 3.14 ;
+     char star = '*';
+     printf("age is %d",age);        showing output 
+     printf("\nvalue of pi %f",pi);
+     printf("\nstar is (%c)asteras",star);
+     return 0; */
+
+    /* int age ;
+     printf("enter a age ");
+     scanf("%d",&age);
+     printf("age is %d",age);
+     return 0 ; */
+
+     int a , b ;
+     printf("enter a ");
+     scanf("%d",&a);
+
+     printf("enter b ");
+     scanf("%d",&b);
+   //int sum = a + b ;  // here we also use 3rd variable ;
+     printf("\nsum is %d",a + b );
+     printf("\nsub is %d",a - b );
+     printf("\nmul of two nu is %d",a * b );
+     printf("\ndivison of two no = %d " , a / b );
+     return 0 ; 
+
+}    
