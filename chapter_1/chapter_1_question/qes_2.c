3801f0d55dc8a4e429a31a7923440d2334a4a376
Shivam-Jais <sivamjaiswal322@gmail.com>
2025-05-18 12:46:11 +0530

    Delete chapter_1 directory

--- chapter_1/chapter_1_question/qes_2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main (){
-    int n ; 
-    printf("enter a number "); 
-    scanf("%d",&n);
-    printf("the cube of n is %d",n*n*n);
-    return 0;
-}
--- chapter_1/hel.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main (){
-    printf("suchii");
-    return 0 ;
-    
-}
